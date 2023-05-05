a749789013435ee347cc5957bf9c594ac342dfc5
AMBergin <AMBergin2002@gmail.com>
2023-05-05 10:36:06 +0100

    Update Assignment 2 Code.py

--- Assignment 2 Code.py
+++ Assignment 2 Code.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,7 +8,7 @@ def metropolis(beta, n, x_0=0):
     H = x**2
     HList = [H]
     for i in range(n):
-        displacement = np.random.normal(0, 1)
+        displacement = np.random.normal(-1, 1)
         x_1 = x+displacement #candidate
         H_1 = x_1**2 #candidate energy
         DeltaH = H_1 - H 
@@ -22,36 +23,28 @@ def metropolis(beta, n, x_0=0):
         # at the beginning of this function
     return np.array(HList)
 
-def energy(beta, n, N): #n is the number of steps and N is the number of samples taken
-    Hs = np.zeros(N) 
+def energy(beta1, beta2, n, N):
+    Hs = np.zeros(N)
+    HReweight = np.zeros(N)
     for i in range(N):
-        HList = metropolis(beta, n)
+        HList = metropolis(beta1, n)
         Hs[i] = np.mean(HList)
-    return np.mean(Hs), np.std(Hs) #this collects the mean of the results ensuring it is as accurate as possible
+        weights = np.exp(-(beta2 - beta1) * HList)
+        HReweight[i] = np.mean(HList * weights) / np.mean(weights)
+    return np.mean(Hs), np.std(Hs), np.mean(HReweight), np.std(HReweight) #this collects the mean of the results ensuring it is as accurate as possible
 
-def reweight(Beta1, Beta2, E1, P1):
-    P2 = np.sum(np.exp(-Beta1 * metropolis(Beta2, times)))
-    E2 = np.sum(metropolis(Beta2, times) * np.exp(-Beta2 * metropolis(Beta2, times))) / P2
-    return E1 * np.exp((Beta1 - Beta2) * E1) / P1 * np.exp((Beta2 - Beta1) * E2) / P2
- #this is the reweighting procedure using the formula provided
+Beta1List = np.linspace(1, 10, 100)
+Hs = np.zeros(len(Beta1List))
+HReweight = np.zeros(len(Beta1List))
+DErrors = np.zeros(len(Beta1List))
+RErrors = np.zeros(len(Beta1List))
+for i, beta1 in enumerate(Beta1List):
+    Hs[i], DErrors[i], HReweight[i], RErrors[i] = energy(beta1, 1+i/10, times, 10)
+ #this compiles all results into a single list for plotting
+    
 
-BetaList= np.linspace(1, 10, 100)
-Hs= np.zeros(len(BetaList))
-HReweight = np.zeros(len(BetaList))
-DErrors = np.zeros(len(BetaList))
-RErrors = np.zeros(len(BetaList))
-for i, beta in enumerate(BetaList):
-    Hs[i], DErrors[i]= energy(beta, times, 10) #this compiles all results into a single list for plotting
-    if i == 0: #this compiles the reweighted data for plotting
-        P1= np.sum(np.exp(-BetaList[i]*metropolis(BetaList[i], times)))
-        HReweight[i]=Hs[i] 
-        RErrors[i] = DErrors[i]
-    else:
-        HReweight[i]=reweight(BetaList[i-1], BetaList[i],HReweight[i-1], P1)
-        RErrors[i] = DErrors[i-1] * np.sqrt(np.exp(-2 * (BetaList[i] - BetaList[i-1]) * HReweight[i-1] / P1))
-
-plt.errorbar(BetaList, Hs, yerr=DErrors, fmt='o', label='Direct')
-plt.errorbar(BetaList, HReweight, yerr=RErrors, fmt='o', label='Reweight')
+plt.errorbar(Beta1List, Hs, yerr=DErrors, fmt='o', label='Direct')
+plt.errorbar(Beta1List, HReweight, yerr=RErrors, fmt='o', label='Reweight')
 plt.xlabel('Inverse Temperature (Beta)')
 plt.ylabel('Average Energy (E)')
 plt.title('Gaussian System with Hamiltonian H=x^2')
